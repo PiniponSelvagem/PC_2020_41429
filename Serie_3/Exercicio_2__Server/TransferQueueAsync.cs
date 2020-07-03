c8449d5a858e4faa3279b5ac33ad73e81512ac14
PiniponSelvagem <piniponselvagem@gmail.com>
2020-07-03 17:32:19 +0100

    Added comments to TransferQueueAsync

--- Serie_3/Exercicio_2__Server/TransferQueueAsync.cs
+++ Serie_3/Exercicio_2__Server/TransferQueueAsync.cs
@@ -6,7 +6,28 @@ using System.Threading;
 using System.Threading.Tasks;
 
 namespace Exercicio_2__Server {
-    class TransferQueueAsync<T> where T : class {
+    
+    /*
+     * ------------------------------------------------------------------------------
+     * DEVELOPER NOTE
+     * ------------------------------------------------------------------------------
+     * 
+     * Esta classe 'TransferQueueAsync' foi realizada em conjunto com o
+     * meu colega Guilherme Salvador [43543] de PC.
+     * Por essa razão deverá haver partes do código nesta classe que estarão
+     * parecidas/iguais.
+     * O meu colega foi quem estava a perceber melhor esta parte final da
+     * matéria, 'Sincronizadores Async', e eu apenas limitei-me a tentar perceber
+     * o código, a comentar, questionar e contra propor a direcção desta
+     * implementação.
+     * 
+     * Posto isto, os exercícios das Série 1 e 2 mais o exercício 1 desta Série 3,
+     * são da minha autoria e deste modo estarei mais a par das suas implementações.
+     * 
+     * ------------------------------------------------------------------------------
+     */
+
+     class TransferQueueAsync<T> where T : class {
 
         // Base type for the two async requests
         private class AsyncRequest<V> : TaskCompletionSource<V> {
@@ -86,167 +107,235 @@ namespace Exercicio_2__Server {
 
 
         public TransferQueueAsync() {
+            // Construct delegates used to describe the cancellation handlers.
             cancellationTakeHandler = new Action<object>((node) => RequestCancellationTakeHandler(node, true));
             timeoutTakeHandler = new TimerCallback((node) => RequestCancellationTakeHandler(node, false));
             cancellationTransferHandler = new Action<object>((node) => RequestCancellationTransferHandler(node, true));
             timeoutTransferHandler = new TimerCallback((node) => RequestCancellationTransferHandler(node, false));
 
+            // Initialize the shared mutable state
             pendingMessages = new LinkedList<Message>();
             asyncTakes = new LinkedList<AsyncTake>();
         }
 
 
 
+        /**
+	     * Auxiliary methods, related to 'Take'
+	     */
 
+        /**
+         * Returns the list of all pending async takes that can be satisfied.
+         *
+         * Note: This method is called when the current thread owns the lock.
+         */
         private List<AsyncTake> SatisfyPendingAsyncTakes() {
             List<AsyncTake> satisfied = null;
             while (asyncTakes.Count > 0) {
                 AsyncTake take = asyncTakes.First.Value;
 
-                if (pendingMessages.Count == 0) {
+                // Check if available pendingmessages allow satisfy this async take
+                if (pendingMessages.Count == 0)
                     break;
-                }
 
+                // Remove the async take from the queue
                 asyncTakes.RemoveFirst();
 
+                // Update take and mark message as done
                 Message msg = pendingMessages.First.Value;
                 take.message = msg.message;
                 if (msg.transfer != null) {
                     msg.transfer.done = true;
                 }
                 pendingMessages.RemoveFirst();
-
                 take.done = true;
 
-                if (satisfied == null) {
+                // Add the async take to the result list
+                if (satisfied == null)
                     satisfied = new List<AsyncTake>();
-                }
-
                 satisfied.Add(take);
             }
             return satisfied;
         }
 
+        /**
+	     * Complete the tasks associated to the satisfied async takes.
+	     *
+	     *  Note: This method is called when the current thread **does not own the lock**.
+	     */
         private void CompleteSatisfiedAsyncTakes(List<AsyncTake> toComplete) {
             if (toComplete != null) {
                 foreach (AsyncTake take in toComplete) {
                     T msg = take.message;
+                    // Dispose the resources associated with the async take and
+                    // complete its task with success.
                     take.Dispose();
-                    take.SetResult(msg);
+                    take.SetResult(msg);    // complete the associated take's task
                 }
             }
         }
 
+        /**
+	     * Try to cancel an async acquire request
+	     */
         private void RequestCancellationTakeHandler(object _takeNode, bool cancelling) {
             LinkedListNode<AsyncTake> takeNode = (LinkedListNode<AsyncTake>) _takeNode;
             AsyncTake take = takeNode.Value;
             bool complete = false;
             List<AsyncTake> satisfied = null;
 
+            // To access shared mutable state we must acquire the lock
             lock (theLock) {
+
+                /**
+			     * Here, the async take can be already satisfied or cancelled.
+			     */
                 if (!take.done) {
+                    // Remove the async acquire take from queue and mark it as done.
                     asyncTakes.Remove(takeNode);
                     complete = take.done = true;
 
+                    // If after removing the async take is possible to satisfy any
+                    // pending async take(s) do it 
                     if (pendingMessages.Count > 0) {
                         satisfied = SatisfyPendingAsyncTakes();
                     }
                 }
             }
 
+            // If we cancelled the async take, release the resources associated with it,
+            // and complete the underlying task.
             if (complete) {
+                // Complete any satisfied async takes
                 if (satisfied != null) {
                     CompleteSatisfiedAsyncTakes(satisfied);
                 }
 
+                // Dispose the resources associated with the cancelled async take
                 take.Dispose(cancelling);
 
+                // Complete the TaskCompletionSource to RanToCompletion with false (timeout)
+                // or Canceled final state (cancellation).
                 if (cancelling) {
-                    take.SetCanceled();
+                    take.SetCanceled();     // cancelled
                 }
                 else {
-                    take.SetResult(null);
+                    take.SetResult(null);   // timeout
                 }
             }
         }
 
 
 
+        /**
+	     * Auxiliary methods, related to 'Message'
+	     */
 
+        /**
+         * Returns the list of all pending async messages that can be satisfied.
+         *
+         * Note: This method is called when the current thread owns the lock.
+         */
         private List<Message> SatisfyPendingMessages() {
             List<Message> satisfied = null;
             while (pendingMessages.Count > 0) {
                 Message msg = pendingMessages.First.Value;
 
-                if (asyncTakes.Count == 0) {
+                // Check if available asyncTakes allow satisfy this async message
+                if (asyncTakes.Count == 0)
                     break;
-                }
 
+                // Remove the pending message from the queue
                 pendingMessages.RemoveFirst();
 
+                // Update take and mark message as done
                 AsyncTake take = asyncTakes.First.Value;
                 take.message = msg.message;
 
                 asyncTakes.RemoveFirst();
 
-                if (msg.transfer != null) {
+                if (msg.transfer != null)
                     msg.transfer.done = true;
-                }
 
-                if (satisfied == null) {
+                // Add the async acquire to the result list
+                if (satisfied == null)
                     satisfied = new List<Message>();
-                }
                 satisfied.Add(msg);
             }
             return satisfied;
         }
 
+        /**
+	     * Complete the messages associated to the satisfied async transfers.
+	     *
+	     *  Note: This method is called when the current thread **does not own the lock**.
+	     */
         private void CompleteSatisfiedMessages(List<Message> toComplete) {
             if (toComplete != null) {
                 foreach (Message msg in toComplete) {
+                    // Dispose the resources associated with the async message and
+                    // complete its task with success.
                     msg.transfer.Dispose();
-                    msg.transfer.SetResult(true);
+                    msg.transfer.SetResult(true);   // complete the associated message's task
                 }
             }
         }
 
+        /**
+	     * Try to cancel an async acquire request
+	     */
         private void RequestCancellationTransferHandler(object _msgNode, bool cancelling) {
             LinkedListNode<Message> msgNode = (LinkedListNode<Message>) _msgNode;
             Message msg = msgNode.Value;
             bool complete = false;
             List<Message> satisfied = null;
 
+            // To access shared mutable state we must acquire the lock
             lock (theLock) {
+                /**
+			     * Here, the async transfer can be already satisfied or cancelled.
+			     */
                 if (!msg.transfer.done) {
+                    // Remove the async transfer request from queue and mark it as done.
                     pendingMessages.Remove(msgNode);
                     complete = msg.transfer.done = true;
 
+                    // If after removing the async acquire is possible to satisfy any
+                    // pending async transfer(s) do it 
                     if (pendingMessages.Count > 0) {
                         satisfied = SatisfyPendingMessages();
                     }
                 }
             }
 
+            // If we cancelled the async transfer, release the resources associated with it,
+            // and complete the underlying task.
             if (complete) {
+                // Complete any satisfied async transfer
                 if (satisfied != null) {
                     CompleteSatisfiedMessages(satisfied);
                 }
 
+                // Dispose the resources associated with the cancelled async transfer
                 msg.transfer.Dispose(cancelling);
 
+                // Complete the TaskCompletionSource to RanToCompletion with false (timeout)
+                // or Canceled final state (cancellation).
                 if (cancelling) {
-                    msg.transfer.SetCanceled();
+                    msg.transfer.SetCanceled();     // cancelled
                 }
                 else {
-                    msg.transfer.SetResult(false);
+                    msg.transfer.SetResult(false);  // timeout
                 }
             }
         }
 
 
 
-
-
+        /**
+	     * Asynchronous Task-based Asynchronous Pattern (TAP) interface.
+	     */
+         
         public void PutAsync(T message) {
             lock (theLock) {
                 if (asyncTakes.Count > 0) {
